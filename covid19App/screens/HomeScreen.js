65b42c9074c7a095d14c287e33a135848e0602d1
Mathias Boampong <mathiasboampong123@gmail.com>
2020-06-21 11:53:52 +0000

    working on home

--- covid19App/screens/HomeScreen.js
+++ covid19App/screens/HomeScreen.js
@@ -130,10 +130,131 @@ export default function HomeScreen() {
               </View>
               <View style={{ alignItems: 'flex-end' }}>
                 <Text style={{ marginTop: 15, fontSize: RFValue(15) }}>
-                  As at the morning of 25 March 2020, a total of sixty-eight(68) cases including two
-                  deaths have been confirmed.Sixty-six(66) of these confirmed cases are being
-                  managed in isolation. The sudden spike in case incidence is as a result of the
-                  mandatory quarantine and compulsory testing for all travellers
+                  As at the morning of 25 March 2020, a total of sixty-eight (68) cases including
+                  two (2) deaths have been confirmed. Sixty-six (66) of these confirmed cases are
+                  being managed in isolation. The sudden spike in case incidence is as a result of
+                  the mandatory quarantine and compulsory testing for all travelers entering Ghana,
+                  as directed by the president. Overall, 30 of the 68 cases have been reported in
+                  the general population with the remaining 38 cases among persons currently under
+                  mandatory quarantine. As of 24 March, total of 1,030 persons are under mandatory
+                  quarantine; samples from 863 of them have been tested and 38 confirmed positive.
+                  Great majority of the confirmed cases are Ghanaians, who returned home from
+                  affected countries. Seven (7) are of other nationalities namely: Norway, Lebanon,
+                  China and UK. In respect of contact tracing, a total of 829 contacts have been
+                  identified and are being tracked. Total of 826 contacts have been enlisted and
+                  being tracked. Nineteen (19) people have completed the 14 days of mandatory follow
+                  up.
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={{ fontSize: RFValue(19), letterSpacing: 0, fontWeight: 'bold' }}>
+              Ghana's Situation Update
+            </Text>
+            <Text style={{ fontSize: RFValue(15), letterSpacing: 0 }}>Last Updated: 4/16/2020</Text>
+            <View style={{ marginTop: 20, alignItems: 'flex-end', flex: 1 }}>
+              <View style={{ borderBottomWidth: 1, borderBottomColor: '#cecece' }}>
+                <Text
+                  style={{
+                    fontSize: RFValue(19),
+                    fontWeight: 'bold',
+                    paddingBottom: 15,
+                  }}>
+                  Confirmed Covid-19 Cases In Ghana As At 25 March 2020, 09:00 Hr
+                </Text>
+              </View>
+              <View style={{ alignItems: 'flex-end' }}>
+                <Text style={{ marginTop: 15, fontSize: RFValue(15) }}>
+                  As at the morning of 25 March 2020, a total of sixty-eight (68) cases including
+                  two (2) deaths have been confirmed. Sixty-six (66) of these confirmed cases are
+                  being managed in isolation. The sudden spike in case incidence is as a result of
+                  the mandatory quarantine and compulsory testing for all travelers entering Ghana,
+                  as directed by the president. Overall, 30 of the 68 cases have been reported in
+                  the general population with the remaining 38 cases among persons currently under
+                  mandatory quarantine. As of 24 March, total of 1,030 persons are under mandatory
+                  quarantine; samples from 863 of them have been tested and 38 confirmed positive.
+                  Great majority of the confirmed cases are Ghanaians, who returned home from
+                  affected countries. Seven (7) are of other nationalities namely: Norway, Lebanon,
+                  China and UK. In respect of contact tracing, a total of 829 contacts have been
+                  identified and are being tracked. Total of 826 contacts have been enlisted and
+                  being tracked. Nineteen (19) people have completed the 14 days of mandatory follow
+                  up.
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={{ fontSize: RFValue(19), letterSpacing: 0, fontWeight: 'bold' }}>
+              Ghana's Situation Update
+            </Text>
+            <Text style={{ fontSize: RFValue(15), letterSpacing: 0 }}>Last Updated: 4/16/2020</Text>
+            <View style={{ marginTop: 20, alignItems: 'flex-end', flex: 1 }}>
+              <View style={{ borderBottomWidth: 1, borderBottomColor: '#cecece' }}>
+                <Text
+                  style={{
+                    fontSize: RFValue(19),
+                    fontWeight: 'bold',
+                    paddingBottom: 15,
+                  }}>
+                  Confirmed Covid-19 Cases In Ghana As At 25 March 2020, 09:00 Hr
+                </Text>
+              </View>
+              <View style={{ alignItems: 'flex-end' }}>
+                <Text style={{ marginTop: 15, fontSize: RFValue(15) }}>
+                  As at the morning of 25 March 2020, a total of sixty-eight (68) cases including
+                  two (2) deaths have been confirmed. Sixty-six (66) of these confirmed cases are
+                  being managed in isolation. The sudden spike in case incidence is as a result of
+                  the mandatory quarantine and compulsory testing for all travelers entering Ghana,
+                  as directed by the president. Overall, 30 of the 68 cases have been reported in
+                  the general population with the remaining 38 cases among persons currently under
+                  mandatory quarantine. As of 24 March, total of 1,030 persons are under mandatory
+                  quarantine; samples from 863 of them have been tested and 38 confirmed positive.
+                  Great majority of the confirmed cases are Ghanaians, who returned home from
+                  affected countries. Seven (7) are of other nationalities namely: Norway, Lebanon,
+                  China and UK. In respect of contact tracing, a total of 829 contacts have been
+                  identified and are being tracked. Total of 826 contacts have been enlisted and
+                  being tracked. Nineteen (19) people have completed the 14 days of mandatory follow
+                  up.
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={{ fontSize: RFValue(19), letterSpacing: 0, fontWeight: 'bold' }}>
+              Ghana's Situation Update
+            </Text>
+            <Text style={{ fontSize: RFValue(15), letterSpacing: 0 }}>Last Updated: 4/16/2020</Text>
+            <View style={{ marginTop: 20, alignItems: 'flex-end', flex: 1 }}>
+              <View style={{ borderBottomWidth: 1, borderBottomColor: '#cecece' }}>
+                <Text
+                  style={{
+                    fontSize: RFValue(19),
+                    fontWeight: 'bold',
+                    paddingBottom: 15,
+                  }}>
+                  Confirmed Covid-19 Cases In Ghana As At 25 March 2020, 09:00 Hr
+                </Text>
+              </View>
+              <View style={{ alignItems: 'flex-end' }}>
+                <Text style={{ marginTop: 15, fontSize: RFValue(15) }}>
+                  As at the morning of 25 March 2020, a total of sixty-eight (68) cases including
+                  two (2) deaths have been confirmed. Sixty-six (66) of these confirmed cases are
+                  being managed in isolation. The sudden spike in case incidence is as a result of
+                  the mandatory quarantine and compulsory testing for all travelers entering Ghana,
+                  as directed by the president. Overall, 30 of the 68 cases have been reported in
+                  the general population with the remaining 38 cases among persons currently under
+                  mandatory quarantine. As of 24 March, total of 1,030 persons are under mandatory
+                  quarantine; samples from 863 of them have been tested and 38 confirmed positive.
+                  Great majority of the confirmed cases are Ghanaians, who returned home from
+                  affected countries. Seven (7) are of other nationalities namely: Norway, Lebanon,
+                  China and UK. In respect of contact tracing, a total of 829 contacts have been
+                  identified and are being tracked. Total of 826 contacts have been enlisted and
+                  being tracked. Nineteen (19) people have completed the 14 days of mandatory follow
+                  up.
                 </Text>
               </View>
             </View>
