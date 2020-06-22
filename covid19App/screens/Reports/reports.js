67359bb6eb651f2b79d815a660d05f41272a939a
Mathias Boampong <mathiasboampong123@gmail.com>
2020-06-22 10:55:21 +0000

    report screen done

--- covid19App/screens/Reports/reports.js
+++ covid19App/screens/Reports/reports.js
@@ -22,7 +22,41 @@ export default function Reports(props) {
           <Text style={{ fontSize: RFValue(25), fontWeight: 'bold' }}>Case Reports</Text>
         </View>
       </View>
-      <View></View>
+      <View style={{ marginTop: RFValue(70), justifyContent: 'center', alignItems: 'center' }}>
+        <View>
+          <Ionicons name={'ios-document'} color={'#dbdbdb'} size={RFValue(120)} />
+          <View
+            style={{
+              backgroundColor: '#bdbdbd',
+              width: 50,
+              height: 50,
+              borderRadius: 50,
+              position: 'absolute',
+              right: 30,
+              top: -10,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ fontSize: RFValue(50), fontWeight: '500' }}>?</Text>
+          </View>
+        </View>
+        <View style={{ marginTop: 40 }}>
+          <Text>You have not made any case reports</Text>
+          <TouchableOpacity
+            style={{
+              height: 60,
+              borderWidth: 1,
+              borderColor: 'black',
+              marginTop: 15,
+              borderRadius: 5,
+              borderStyle: 'dashed',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>Make Case Report</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
